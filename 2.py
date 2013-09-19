1af0a299b4e73ed6c456e2687ff9a2380102fc22
benemery <ben.s.emery@gmail.com>
2013-09-19 21:10:49 +0100

    Tidy up

--- 2.py
+++ 2.py
@@ -2,14 +2,14 @@ from toolbox import fib
 from itertools import takewhile
 
 def test():
-	# No test given
-	return True
+    # No test given
+    return True
 
 def main():
-	fibs = takewhile(lambda x: x<4e6, fib())
-	even_fibs = [n for n in fibs if n%2==0]
-	return sum(even_fibs)
+    fibs = takewhile(lambda x: x<4e6, fib())
+    even_fibs = [n for n in fibs if n%2==0]
+    return sum(even_fibs)
 
-if __name__ == '__main__':	
-	print "Passes test? ", test()
-	print "Answer: ", main()
+if __name__ == '__main__':
+    print "Passes test? ", test()
+    print "Answer: ", main()
--- toolbox.py
+++ toolbox.py
@@ -1,18 +1,19 @@
+from itertools import takewhile
+
 def fib():
-	'''
-		Returns a generator for the Fibonacci numbers
-	'''
-	first, second = 0, 1
-	while True:
-		yield first
-		first, second = first + second, first
+    """ Returns a generator for the Fibonacci numbers """
+    first, second = 0, 1
+    while True:
+        yield first
+        first, second = first + second, first
+
 def largest_prime_divisor(n):
-	""" Find the largest prime divisor of a number """
-	i = 1
-	while i * i < n:  # sqrt of n is the upper limit
-	    if n%i == 0:  # we have a divisor, lower n to the larger divisor
-	        n = n / i
-	    i += 1
-	# we have continually set n to the largest of two divisors, what remains
-	# is the largest divisor, which has no other divisors, and is therefore prime!
-	return n
+    """ Find the largest prime divisor of a number """
+    i = 1
+    while i * i < n:  # sqrt of n is the upper limit
+        if n%i == 0:  # we have a divisor, lower n to the larger divisor
+            n = n / i
+        i += 1
+    # we have continually set n to the largest of two divisors, what remains
+    # is the largest divisor, which has no other divisors, and is therefore prime!
+    return n
