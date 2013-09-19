0202a69c57b104fdfea61c696671b7f53bac18f0
benemery <ben.s.emery@gmail.com>
2013-09-19 21:06:27 +0100

    Problem 3

---
+++ 3.py
@@ -0,0 +1,11 @@
+from toolbox import largest_prime_divisor
+
+def test():
+	return largest_prime_divisor(13195) == 29 
+
+def main():
+	return largest_prime_divisor(600851475143)
+
+if __name__ == '__main__':
+	print "Passes test? ", test()
+	print "Answer: ", main()
