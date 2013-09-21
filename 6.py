3511d2cc7b6e3777e93ee1de5ddba18085a8544d
Ben Emery <ben.s.emery@gmail.com>
2013-09-21 18:59:28 +0100

    Problem 6

---
+++ 6.py
@@ -0,0 +1,13 @@
+def sum_square_difference_for_range(x, y):
+    nums = range(x, y + 1)
+    return sum(nums) * sum(nums) - sum(map(lambda x: x*x, nums))
+
+def test():
+    return sum_square_difference_for_range(1, 10) == 2640
+
+def main():
+    return sum_square_difference_for_range(1, 100)
+
+if __name__ == '__main__':
+    print "Passes test? ", test()
+    print "Answer: ", main()
