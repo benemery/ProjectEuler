de820d37f564919d57b98489743ff2bf21ca4d9d
Ben Emery <ben.s.emery@gmail.com>
2013-09-22 11:50:45 +0100

    Problem 7

---
+++ 7.py
@@ -0,0 +1,18 @@
+from toolbox import gen_primes
+
+def nth_prime(n):
+    counter = 1
+    for prime in gen_primes():
+        if counter == n:
+            return prime
+        counter += 1
+
+def test():
+    return nth_prime(6) == 13
+
+def main():
+    return nth_prime(10001)
+
+if __name__ == '__main__':
+    print "Passes test? ", test()
+    print "Answer: ", main()
