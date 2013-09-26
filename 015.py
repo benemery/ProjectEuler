2bcc511074808b14cfc38d91689ed5bd408756de
Ben Emery <ben.s.emery@gmail.com>
2013-09-26 18:03:49 +0100

    Problem 15

---
+++ 015.py
@@ -0,0 +1,17 @@
+from toolbox import nCr
+
+def lattice_paths(x, y):
+    # we need to take x+y steps to reach one corner of the lattice to the other.
+    # if we took x+y steps horizontally we'd be pretty off target.
+    # thus we need to choose y steps to flip to being vertical.
+    return nCr(x+y,y)
+
+def test():
+    return lattice_paths(2, 2) == 6
+
+def main():
+    return lattice_paths(20, 20)
+
+if __name__ == '__main__':
+    print "Passes test? ", test()
+    print "Answer: ", main()
