f5d99f3c819f0c00b4906a7bfefc500084e5427e
benemery <ben.s.emery@gmail.com>
2013-10-01 19:21:36 +0100

    Problem 20. One liner :D

---
+++ 020.py
@@ -0,0 +1,14 @@
+from math import factorial as f
+
+def sum_factorial(n):
+    return sum(map(int, list(str(f(n)))))
+
+def test():
+    return sum_factorial(10) == 27
+
+def main():
+    return sum_factorial(100)
+
+if __name__ == '__main__':
+    print "Passes test? ", test()
+    print "Answer: ", main()
