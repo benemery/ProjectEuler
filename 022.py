4a6d76a5290383bebe30bdff2e668fa022100d6c
benemery <ben.s.emery@gmail.com>
2013-10-03 19:23:32 +0100

    Problem 22

---
+++ 022.py
@@ -0,0 +1,19 @@
+def test():
+    # Test is pretty poor
+    return True
+
+def main():
+    names = open('022_names.txt', 'r').read()
+    # turn to a list of names
+    names = names.replace('"', '').split(',')
+    names.sort()
+
+    answer = 0
+    for index, name in enumerate(names):
+        # NOTE: the answer is indexed from one whereas enumerate is indexed at 0
+        answer += (index + 1) * sum([ord(letter) - 64 for letter in list(name)])
+    return answer
+
+if __name__ == '__main__':
+    print "Passes test? ", test()
+    print "Answer: ", main()
