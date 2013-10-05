09b78a1e207d9f859b0fa7dfaa8392c894c22fd2
benemery <ben.s.emery@gmail.com>
2013-10-05 12:11:56 +0100

    Problem 24

---
+++ 024.py
@@ -0,0 +1,14 @@
+from itertools import permutations
+
+def test():
+    return True
+
+def main():
+    for i, p in enumerate(permutations('0123456789')):
+        if i == 1e6 - 1:
+            return ''.join(p)
+
+
+if __name__ == '__main__':
+    print "Passes test? ", test()
+    print "Answer: ", main()
