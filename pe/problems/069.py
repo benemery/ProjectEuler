056f59ba290dd365a2893c24636af6970bae3fdf
Ben Emery <ben.s.emery@gmail.com>
2015-03-09 08:49:58 +0000

    Fast solution for 69 complete.

--- pe/problems/069.py
+++ pe/problems/069.py
@@ -1,7 +1,7 @@
 from math import ceil
 import fractions
 
-from pe.toolbox import totient, totient_1
+from pe.toolbox import totient, totient_1, gen_primes
 
 
 def find_largest_ratio(limit):
@@ -65,18 +65,46 @@ def find_largest_ratio_3(limit):
     return winner
 
 
+def find_largest_ratio_4(limit):
+    """We're looking to find the largest value for n / phi(n).
+
+    This means that we want to minimise phi(n). Let's look at some
+    properties of phi(n).
+
+    Phi is mulitiplicitive and n can written in terms of prime divisors,
+    ie.
+
+    phi(n)  = phi(p_1 * p_2 * ... * p_n)
+            = phi(p_1) * phi(p_2) * .. * phi(p_n)
+
+    and
+
+    phi(p^k) = p^(k-1) * (p - 1)
+
+    then
+
+    phi(n)  = (p_1 - 1) * (p_2 - 1) * .. * (p_n - 1), for k = 1.
+
+    where p is prime.
+
+    Using this we can see that the lower bound for phi(n) is found for
+    numbers that have single power, small prime factors,
+    """
+    ans = 1
+    ans_new = ans
+    for prime in gen_primes():
+        ans_new *= prime
+        if ans_new > limit:
+            return ans
+        ans = ans_new
+
+
 def test():
-    return find_largest_ratio(10) == 6
+    return find_largest_ratio_4(10) == 6
 
 def main():
-    import time
     N = 1000000
-    # start = time.time()
-    # x = find_largest_ratio_2(N)
-    # print time.time() - start, x
-    start = time.time()
-    x = find_largest_ratio_3(N)
-    print time.time() - start, x
+    x = find_largest_ratio_4(N)
     return x
 
 if __name__ == '__main__':
